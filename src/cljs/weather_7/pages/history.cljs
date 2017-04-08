d2e3fbfe53d2bc43158fdc39f3e3abf73609dead
mikeyjcat <mike@hewitson.co.za>
2017-04-08 10:31:06 +0200

    fix: remove yaxis minimums to allow lines to align

--- src/cljs/weather_7/pages/history.cljs
+++ src/cljs/weather_7/pages/history.cljs
@@ -17,11 +17,13 @@
 (defonce chart-config-static
   {:chart {:type "spline"}
    :yAxis [
-           {:min 0
+           {
+            ; :min 0
             :title {:text "Degrees C"
                     :align "high"}
             :labels {:overflow "justify"}}
-           {:min 0
+           {
+            ; :min 0
             :title {:text "Km/hr"
                     :align "high"}
             :labels {:overflow "justify"}
