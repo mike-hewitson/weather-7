ab9e45c7f78284a57857a8e5022049afaf6c1124
Mike Hewitson <mike@hewitson.co.za>
2017-04-18 13:52:39 +0200

    test: added some tests for home page and fixed bugs

--- src/cljs/weather_7/pages/home.cljs
+++ src/cljs/weather_7/pages/home.cljs
@@ -9,14 +9,18 @@
 (def date-format (tf/formatter "HH:mm"))
 (def date-time-format (tf/formatter "yyyy:MM:dd HH:mm"))
 
+(defn days-to-next-spring
+  [age-of-moon]
+  (- 14 (mod age-of-moon 14)))
+
 (defn format-moon-phase-test
   "create text for moon phase"
   [age-of-moon]
-  (let [next-moon-type (if (quot age-of-moon 14)
+  (let [next-moon-type (if (= (quot age-of-moon 14) 0)
                          "full moon"
                          "new moon")
-        days-to-next-spring (mod 14 age-of-moon)]
-    (if (= days-to-next-spring 0)
+        days-to-next-spring (days-to-next-spring age-of-moon)]
+    (if (= days-to-next-spring 14)
       next-moon-type
       (str days-to-next-spring
            (if (= days-to-next-spring 1)
@@ -24,8 +28,9 @@
              " days to ")
            next-moon-type))))
 
-(defn create-reading-element [reading]
- (let [days-to-next-spring (mod 14 (:age-of-moon reading))]
+(defn create-reading-element
+  [reading]
+ ; (let [days-to-next-spring (mod 14 (:age-of-moon reading))]
   [:div.row
    [:h4 (str (:location reading) " ")
     [:i {:class (str "wi " (:icon reading))}]
@@ -76,9 +81,8 @@
          (:type reading) " "
          (gstring/format "%.1f" (:height reading)) " m at "
          (tf/unparse date-format (t/to-default-time-zone (:date reading)))
-         (if (= days-to-next-spring 0)
-           ": spring tide")]])]]]))
-
+         (if (= (days-to-next-spring (:age-of-moon reading)) 14)
+           ": spring tide")]])]]])
 
 ; TODO create tests for this stuff
 
--- test/cljs/weather_7/core_test.cljs
+++ test/cljs/weather_7/core_test.cljs
@@ -2,10 +2,19 @@
   (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
             [pjstadig.humane-test-output]
             [reagent.core :as reagent :refer [atom]]
-            [weather-7.core :as rc]))
+            [weather-7.core :as rc]
+            [weather-7.pages.home :as ph]))
 
-(deftest test-home
-  (is (= true true)))
+(deftest test-days-to-next-spring
+  (is (= (ph/days-to-next-spring 0) 14))
+  (is (= (ph/days-to-next-spring 5) 9))
+  (is (= (ph/days-to-next-spring 14) 14))
+  (is (= (ph/days-to-next-spring 15) 13)))
 
-(deftest test-other
-  (is (= true false)))
+(deftest test-moon-phase-text
+  (is (= (ph/format-moon-phase-test 0) "full moon"))
+  (is (= (ph/format-moon-phase-test 5) "9 days to full moon"))
+  (is (= (ph/format-moon-phase-test 14) "new moon"))
+  (is (= (ph/format-moon-phase-test 1) "13 days to full moon"))
+  (is (= (ph/format-moon-phase-test 15) "13 days to new moon"))
+  (is (= (ph/format-moon-phase-test 22) "6 days to new moon")))
