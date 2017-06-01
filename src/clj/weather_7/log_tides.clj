fef1866a205e4597c033115342208b6cda7da339
mikeyjcat <mike@hewitson.co.za>
2017-06-01 07:14:39 +0200

    refactor: readable clojure

--- src/clj/weather_7/log_tides.clj
+++ src/clj/weather_7/log_tides.clj
@@ -1,10 +1,10 @@
 (ns weather-7.log-tides
-  (:require [clj-http.client :as client]
+  (:require [clj-http.client       :as client]
             [clojure.tools.logging :as log]
-            [cprop.core :refer [load-config]]
-            [cprop.source :as source]
-            [monger.core :as mg]
-            [monger.collection :as mc]))
+            [cprop.core            :as cprop :refer [load-config]]
+            [cprop.source          :as source]
+            [monger.core           :as mg]
+            [monger.collection     :as mc]))
 
 ; TODO move locations to database
 
@@ -12,9 +12,8 @@
   '(["Paradise Beach" ["-34.0521" "24.5412"]]))
     ; ["London" ["51.317" "0.057"]]))
 
-(defn get-worldtide-data
+(defn get-worldtide-data [[lat lon]]
   "retrieve a set of readings from worldtide.com for a gps location"
-  [[lat lon]]
   (let [my-url (str
                 "https://www.worldtides.info/api?extremes&lat="
                 lat
@@ -23,23 +22,26 @@
                 "&key=9521f994-2075-4c9f-824e-dc4c85fabd8a")]
     (:body (client/get my-url {:as :json}))))
 
-(defn build-tides
-  []
+
+(defn build-tides []
   (map (fn [[location gps]]
          {:location location
           :tides (get-worldtide-data gps)})
        tide-locations))
 
+
 (defonce now
   (new java.util.Date))
 
+
 (defn log-tides []
   (let [uri (:database-url
-             (load-config :merge
-                          [(source/from-system-props)
-                           (source/from-env)]))
+             (cprop/load-config :merge
+                                [(source/from-system-props)
+                                 (source/from-env)]))
         db (:db (mg/connect-via-uri uri))]
     (mc/insert db "tides" {:date now :locations (build-tides)})))
+
 
 (defn -main []
   (log-tides)
