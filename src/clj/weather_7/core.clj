5839ec3a4e946a13fba7d0931184f73c74f7fe9b
mikeyjcat <mike@hewitson.co.za>
2017-06-02 06:50:02 +0200

    refactor: redable clojure

--- src/clj/weather_7/core.clj
+++ src/clj/weather_7/core.clj
@@ -1,26 +1,29 @@
 (ns weather-7.core
-  (:require [weather-7.handler :as handler]
-            [luminus.repl-server :as repl]
-            [luminus.http-server :as http]
-            [weather-7.config :refer [env]]
-            [clojure.tools.cli :refer [parse-opts]]
+  (:require [weather-7.handler     :as handler]
+            [luminus.repl-server   :as repl]
+            [luminus.http-server   :as http]
+            [weather-7.config      :as config :refer [env]]
+            [clojure.tools.cli     :as cli :refer [parse-opts]]
             [clojure.tools.logging :as log]
-            [mount.core :as mount])
+            [mount.core            :as mount])
   (:gen-class))
+
 
 (def cli-options
   [["-p" "--port PORT" "Port number"
     :parse-fn #(Integer/parseInt %)]])
 
+
 (mount/defstate ^{:on-reload :noop}
   http-server
   :start
   (http/start
-   (-> env
+   (-> config/env
        (assoc :handler (handler/app))
        (update :port #(or (-> env :options :port) %))))
   :stop
   (http/stop http-server))
+
 
 (mount/defstate ^{:on-reload :noop}
   repl-server
@@ -31,18 +34,21 @@
   (when repl-server
     (repl/stop repl-server)))
 
+
 (defn stop-app []
   (doseq [component (:stopped (mount/stop))]
     (log/info component "stopped"))
   (shutdown-agents))
 
+
 (defn start-app [args]
   (doseq [component (-> args
-                        (parse-opts cli-options)
+                        (cli/parse-opts cli-options)
                         mount/start-with-args
                         :started)]
     (log/info component "started"))
   (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app)))
+
 
 (defn -main [& args]
   (start-app args))
