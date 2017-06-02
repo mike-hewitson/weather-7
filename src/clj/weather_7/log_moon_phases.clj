1fc064bb9255d6d59b93e1fd14aa8c845d019dd5
mikeyjcat <mike@hewitson.co.za>
2017-06-02 06:32:07 +0200

    refactor: readable clojure

--- src/clj/weather_7/log_moon_phases.clj
+++ src/clj/weather_7/log_moon_phases.clj
@@ -1,10 +1,10 @@
 (ns weather-7.log-moon-phases
-  (:require [clj-http.client :as client]
+  (:require [clj-http.client       :as client]
             [clojure.tools.logging :as log]
-            [cprop.core :refer [load-config]]
-            [cprop.source :as source]
-            [monger.core :as mg]
-            [monger.collection :as mc]))
+            [cprop.core            :as cprop :refer [load-config]]
+            [cprop.source          :as source]
+            [monger.core           :as mg]
+            [monger.collection     :as mc]))
 
 ; TODO move locations to database
 
@@ -14,6 +14,11 @@
     ["Salt River" "ZA/Salt_River"]))
     ; ["London" "UK/London"]))
 
+
+(defonce now
+  (new java.util.Date))
+
+
 (defn get-moon-phase-data [search-string]
   "retrieve a set of readings from wunderorldtide.com for a gps location"
   (let [my-url (str
@@ -22,22 +27,22 @@
                 ".json")]
     (:body (client/get my-url {:as :json}))))
 
+
 (defn build-moon-phases []
   (map (fn [[location search-string]]
          {:location location
           :phases (get-moon-phase-data search-string)})
        moon-locations))
 
-(defonce now
-  (new java.util.Date))
 
 (defn log-moon-phases []
   (let [uri (:database-url
-             (load-config :merge
+             (cprop/load-config :merge
                           [(source/from-system-props)
                            (source/from-env)]))
         db (:db (mg/connect-via-uri uri))]
     (mc/insert db "moon" {:date now :locations (build-moon-phases)})))
+
 
 (defn -main []
   (log-moon-phases)
